632e7333b9057e19cf4461a1a22c6d67c4739ff6
yang_chenwei <786369470@qq.com>
2019-09-10 19:12:53 +0800

    学习

---
+++ java_test/src/normal_test/ArrayTest.java
@@ -0,0 +1,16 @@
+package normal_test;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[][] ls1 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        for(int[] i: ls1){
+            for(int j: i){
+                System.out.println(j);
+            }
+        }
+    }
+}
---
+++ java_test/src/normal_test/CopyTest.java
@@ -0,0 +1,25 @@
+package normal_test;
+
+// 复制数组
+public class CopyTest {
+    public static void main(String[] args){
+        // 复制列表
+//        int[] ls1 =  {1, 2, 3, 4};
+////        // 创建一个长度为3的ls2数组来保存
+//        int[] ls2 = new int[3];
+//        System.arraycopy(ls1, 0, ls2, 0, 3);
+//        for(int i: ls2){
+//            System.out.println(i);
+//        }
+        // 合并列表
+        int[] ls1 = {1, 2, 3, 4};
+        int[] ls2 = {2, 3, 5, 6};
+        int[] new_ls = new int[ls1.length+ls2.length];
+        System.arraycopy(ls1, 0, new_ls, 0, ls1.length);
+        System.arraycopy(ls2, 0, new_ls, ls1.length, ls2.length);
+        // 打印新列表
+        for(int i: new_ls){
+            System.out.println(i);
+        }
+    }
+}
