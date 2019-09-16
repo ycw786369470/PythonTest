164fa468db1ac2b11de62c33b195ff48a7b7a31a
yang_chenwei <786369470@qq.com>
2019-09-16 18:47:32 +0800

    9.16算法题python以及java实现

---
+++ java_test/src/Sorts/HaflSearch.java
@@ -0,0 +1,11 @@
+package Sorts;
+
+public class HaflSearch {
+    public int search(int[] ls, int goal_num, int m, int n){
+        while (m < n){
+//            int mid = (m+n)//2;
+        }
+
+        return 1;
+    }
+}
