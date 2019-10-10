8915ffab4c68139c59a5bac94c01d7c1a15f99ca
yang_chenwei <786369470@qq.com>
2019-10-10 11:41:07 +0800

    数据分析

---
+++ 数据分析/opencv/test.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+import time
+
+img = cv2.imread('add.jpg')
+cv2.imshow('add.jpg', img)
+time.sleep(1)
+
+# white = np.ones((200, 200, 3))
+# cv2.imshow('white', white)
+
